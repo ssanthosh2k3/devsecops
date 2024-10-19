f677b800956402edc4a83af44530d8666da21680
SANTHOSH <114053272+ssanthosh2k3@users.noreply.github.com>
2024-10-19 23:24:59 +0530

    Delete backend  directory

--- backend /models/Test.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const TestSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    date: { type: Date, required: true },
-    type: { type: String, required: true }
-});
-
-module.exports = mongoose.model('Test', TestSchema);
